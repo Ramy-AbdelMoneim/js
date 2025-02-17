6a5fda2041b66655f0a0324745c2f6a50f4f9722
Ramy Elbasha <ramyelbasha2@gmail.com>
2025-02-17 22:40:29 +0200

    .

--- Scripts/LoginScript.js
+++ Scripts/LoginScript.js
@@ -1,50 +1,52 @@
 
 
-let formsubmit=document.forms[0];
-formsubmit.onsubmit=function(event)
-{
+let formsubmit = document.forms[0];
+let cookies = [];
+
+function getcookies() {
+    let allcookies = document.cookie.split(';');
+    for (let i = 0; i < allcookies.length; i++) {
+        var [key, value] = allcookies[i].split('=');
+        cookies[key] = value;
+    }
+    return cookies;
+}
+formsubmit.onsubmit = function (event) {
     event.preventDefault();
-    let Mail=document.getElementById("InputEmail").value;
-    let Password=document.getElementById("InputPassword").value;
-    fetch("../JSON/users.json").then(function(response)
-    {
-        if(!response.ok)
-        {
+    let Mail = document.getElementById("InputEmail").value;
+    let Password = document.getElementById("InputPassword").value;
+    fetch("../JSON/users.json").then(function (response) {
+        if (!response.ok) {
             throw new Error('error')
         }
-        
+
         return response.json()
     }
-    ).then(function(responseText)
-    {
-        let usersdata=responseText;
-        let login=false;
-        for(let i in usersdata)
-        {
-            if(Mail==i && Password==usersdata[i].Password)
-            {
+    ).then(function (responseText) {
+        let usersdata = responseText;
+        let login = false;
+        for (let i in usersdata) {
+            if (Mail == i && Password == usersdata[i].Password) {
                 // console.log("Login Successful")
-                document.cookie=`usrname=${usersdata[i].username}`
-                login=true;
+                document.cookie = `usrname=${usersdata[i].username}`
+                login = true;
                 break;
             }
         }
-        if(login==false)
-        {
+        if (login == false) {
             alert("Invalid Email or Password")
         }
-        else
-        {
-            alert("Login Successful")
+        else {
+            getcookies();
+            alert(`Welcome back, ${cookies.usrname}`)
             formsubmit.submit();
         }
-    
-        
+
+
     }
-    ).catch(function(error)
-    {
+    ).catch(function (error) {
         console.log(error)
     }
     )
-    
+
 }
